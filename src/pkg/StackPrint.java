7de2cefa59fa1e32ee052e91fe8d02d3530d0bc5
gimy boya <b.afatah@yahoo.com>
2017-03-10 20:04:16 +0800

    time calculation++

--- src/pkg/StackPrint.java
+++ src/pkg/StackPrint.java
@@ -8,13 +8,14 @@ import java.util.Stack;
 public class StackPrint implements Runnable {
 
   private Stack<Integer> MyStack = new Stack();
+  private int size;
 
-  public StackPrint(){
-    stackFill(this.MyStack);
+  public StackPrint(int n){
+    this.size = n;
   }
 
   private void stackFill(Stack<Integer> myStack) {
-    for (int i = 0; i < 100; i++) {
+    for (int i = 0; i < this.size; i++) {
       myStack.add((int)(Math.random()*100 + 1));
     }
   }
@@ -22,9 +23,14 @@ public class StackPrint implements Runnable {
   @Override
   public void run() {
 
-    for (int i = 0; i < 100; i++) {
-      System.out.println(MyStack.get(i));
+    long startTime = System.currentTimeMillis();
+    stackFill(this.MyStack);
+    for (int i = 0; i < this.size; i++) {
+      System.out.print(MyStack.get(i) + " ");
     }
+    System.out.println("");
+    long endTime = System.currentTimeMillis();
 
+    System.out.println("Time execution for printing " + this.size + " Elements from the stack is " + (endTime - startTime) + " Ms");
   }
 }
