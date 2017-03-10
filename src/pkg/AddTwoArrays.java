796cf1e4b1dee701d51fb1b81ff4917e2b8181ba
gimy boya <b.afatah@yahoo.com>
2017-03-10 20:04:40 +0800

    add two arrays created

---
+++ src/pkg/AddTwoArrays.java
@@ -0,0 +1,44 @@
+package pkg;
+
+/**
+ * Created by gimy on 3/10/2017.
+ */
+public class AddTwoArrays implements Runnable {
+
+  private int[] ArrayOne;
+  private int[] ArrayTwo;
+  private int size;
+
+  AddTwoArrays(int size){
+    this.size = size;
+    ArrayOne = new int[this.size];
+    ArrayTwo = new int[this.size];
+  }
+
+  private void initializeArray(int[] array) {
+    for (int i = 0; i < array.length; i++) {
+      array[i] = (int)(Math.random() * 100 + 1);
+    }
+  }
+
+  @Override
+  public void run() {
+    long startTime = System.currentTimeMillis();
+
+    initializeArray(ArrayOne);
+    initializeArray(ArrayTwo);
+
+    int[] resultArray = new int[this.size];
+
+    for (int i = 0; i < this.size; i++) {
+      resultArray[i] = this.ArrayOne[i] + this.ArrayTwo[i];
+      System.out.print("(" + ArrayOne[i] + " + " + ArrayTwo[i] + ") = " + resultArray[i] + " ");
+    }
+
+    System.out.println("");
+
+    long endTime = System.currentTimeMillis();
+
+    System.out.println("Time execution for adding two one dimensional arrays with the size " + this.size + " is " + (endTime - startTime) + " Ms");
+  }
+}
--- src/pkg/Main.java
+++ src/pkg/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+      StackPrint taskTwo = new StackPrint(100);
+      AddTwoArrays taskThree = new AddTwoArrays(20);
+
+      Thread threadTwo = new Thread(taskTwo);
+      Thread threadThree = new Thread(taskThree);
+
+      threadTwo.start();
+      threadThree.start();
     }
 }
