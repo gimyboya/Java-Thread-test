2012f173d69c869275751240d1e8711ab4933101
gimy boya <b.afatah@yahoo.com>
2017-03-11 23:36:26 +0800

    linkedlist sort class++

---
+++ src/pkg/LinkedListSort.java
@@ -0,0 +1,17 @@
+package pkg;
+
+import java.util.LinkedList;
+
+/**
+ * Created by gimy on 3/11/2017.
+ */
+public class LinkedListSort implements Runnable {
+
+  private LinkedList<Integer> MyLinkedList;
+
+  
+  @Override
+  public void run() {
+
+  }
+}
